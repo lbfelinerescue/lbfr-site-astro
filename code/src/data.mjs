e2eccfebd0a636d3c1cfe7a3fa3d3e1dcb87e35d
David Nuon <davidnuongm@gmail.com>
2023-04-28 22:59:42 -0700

    Add example of breaking out markup into different components

---
+++ code/src/data.mjs
@@ -0,0 +1,17 @@
+export const slugs = [
+    { 
+        params: {cat: 'mr-mistoffelees'}
+    },
+    {
+        params: {cat: 'rum-tum-tugger'}
+    },
+    {
+        params: {cat: 'grizabella'}
+    },
+    {
+        params: {cat: 'jennyanydots'}
+    },
+    {
+        params: {cat: 'old-deuteronomy'}
+    }
+];
