9f137925fe81aecbbad8ff5f79ecb41b714c5ca0
David Nuon <davidnuongm@gmail.com>
2023-04-30 13:19:44 -0700

    Update package-lock add tina config

---
+++ .tina/config.ts
@@ -0,0 +1,52 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: null, // Get this from tina.io
+  token: null, // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "images",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "posts",
+        label: "Posts",
+        path: "src/content/posts",
+        format: 'mdx',
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "posted",
+            label: "Date Posted",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },  
+        ],
+      },
+    ],
+  },
+});
+
