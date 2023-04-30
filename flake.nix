6b49feccc888876246616ffbe68a3776d02c7392
Daniel Baker <daniel.baker@tweag.io>
2023-04-29 21:36:59 -0700

    migrated bulk of theme to astro.

--- flake.nix
+++ flake.nix
@@ -10,31 +10,33 @@
   inputs.nix-npm-buildpackage.url = "github:serokell/nix-npm-buildpackage";
   inputs.nix-npm-buildpackage.flake = false;
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-compat,
-    flake-utils,
-    npmlock2nix,
-    nix-npm-buildpackage,
-  } @ inputs:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-compat
+    , flake-utils
+    , npmlock2nix
+    , nix-npm-buildpackage
+    ,
+    } @ inputs:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (self: super: {
+              npmlock2nix = pkgs.callPackage npmlock2nix { };
+            })
+          ];
+        };
 
         astro-shell = pkgs.npmlock2nix.v2.shell {
           src = ./.;
           nodejs = pkgs.nodejs;
         };
-
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (self: super: {
-              npmlock2nix = pkgs.callPackage npmlock2nix {};
-            })
-          ];
-        };
-      in {
+      in
+      {
         devShells.astro = astro-shell;
         devShells.default = pkgs.mkShell {
           packages = [
