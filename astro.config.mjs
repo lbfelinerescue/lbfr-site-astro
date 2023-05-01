46adbe61e34df9e7835907a1ebaf701dd27fa32a
Daniel Baker <daniel.baker@tweag.io>
2023-04-30 22:23:20 -0700

    maybe it's working now?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    mdx()
+    mdx(),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+        },
+        collections: [
+          // Content collections
+        ],
+      },
+    }),
   ],
 });
