356a6751fe0257d66e6b121e32c640634370607c
sadiq <mail@sadiq-pasha.com>
2023-05-02 15:09:25 -0700

    Home page VU. Added images. Added navbar to home page. Added tribute section. Add websitemeta metadata file. Added website title and icon.

---
+++ src/websitemeta.js
@@ -0,0 +1,8 @@
+const websitemeta = {
+  title: 'Long Beach Feline Rescue',
+  description: 'A website for Long Beach Feline Rescue',
+  favicon: '/favicon.svg',
+  url: 'https://longbeachfelinerescue.com',
+}
+
+export default websitemeta
