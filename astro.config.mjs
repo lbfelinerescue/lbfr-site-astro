a6c9975ed6f1cc7bd53bfb77be55939c8a5394bc
Daniel Baker <daniel.baker@tweag.io>
2023-05-01 21:25:48 -0700

    having netlify build staging.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
         media_folder: "public/images",
         backend: {
           name: 'git-gateway',
-          branch: 'main',
+          branch: 'staging',
         },
         collections: [
           {
