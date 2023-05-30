d0ab8002d07cc222ea841c0efc85ff126133dbaf
sadiq <mail@sadiq-pasha.com>
2023-05-29 20:04:56 -0700

    Added search engine optimization component. Added it to all layout pages. Deleted "websiteMeta.js". Changed all images to .jpg.

--- src/websitemeta.js
+++
@@ -1,8 +0,0 @@
-const websitemeta = {
-  title: 'Long Beach Feline Rescue',
-  description: 'A website for Long Beach Feline Rescue',
-  favicon: '/favicon.svg',
-  url: 'https://longbeachfelinerescue.com',
-}
-
-export default websitemeta
