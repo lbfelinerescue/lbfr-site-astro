f2c6c21538d250b5361db4054049a4496f424f87
Daniel Baker <daniel.baker@tweag.io>
2023-06-16 23:46:00 -0700

    automated the foster form

--- forms.py
+++ forms.py
@@ -93,7 +93,67 @@ def volunteer(data: Dict) -> None:
 
 
 def foster(data: Dict) -> None:
-    print('foster')
+    driver = webdriver.Firefox()
+    driver.get(data['site'] + '/foster-form/')
+
+    notice_elem = driver.find_element(by=By.XPATH, value='//input[@name="foster-notice"]')
+    js_click(driver, notice_elem)
+
+    driver.find_element(by=By.ID, value='applicant-name').send_keys('Dathanial Autobot')
+    driver.find_element(by=By.ID, value='applicant-occupation').send_keys('Cat Daddy')
+    driver.find_element(by=By.ID, value='applicant-email').send_keys('lbfelinerescue@googlegroups.com')
+    driver.find_element(by=By.ID, value='applicant-phone').send_keys('562-123-4567')
+
+    js_click(driver, driver.find_element(by=By.ID, value='foster-interests-baby-bottle-kitten'))
+    js_click(driver, driver.find_element(by=By.ID, value='foster-interests-teenagers'))
+    js_click(driver, driver.find_element(by=By.ID, value='foster-interests-special-needs-timid'))
+
+    driver.find_element(by=By.ID, value='previous-foster-experience').send_keys('absolutely nothing')
+
+    driver.find_element(by=By.ID, value='applicant-address').send_keys('123 Kitty Lane')
+    driver.find_element(by=By.ID, value='applicant-city').send_keys('Long Beach')
+    driver.find_element(by=By.ID, value='applicant-state').send_keys('CA')
+    driver.find_element(by=By.ID, value='applicant-zip-code').send_keys('90802')
+    driver.find_element(by=By.ID, value='how-long-at-address').send_keys('9 years')
+
+    driver.find_element(by=By.ID, value='other-residents-adults').send_keys('n/a')
+    driver.find_element(by=By.ID, value='other-residents-children').send_keys('n/a')
+
+    js_click(driver, driver.find_element(by=By.ID, value='pet-permission-yes'))
+
+    driver.find_element(by=By.ID, value='current-pets-cats').send_keys('2')
+    driver.find_element(by=By.ID, value='current-pets-dogs').send_keys('0')
+    driver.find_element(by=By.ID, value='current-pets-other').send_keys('1')
+
+    js_click(driver, driver.find_element(by=By.ID, value='current-pet-vaccinations-yes'))
+    js_click(driver, driver.find_element(by=By.ID, value='indoor-only'))
+
+    js_click(driver, driver.find_element(by=By.ID, value='provide-food-and-supplies-yes'))
+    js_click(driver, driver.find_element(by=By.ID, value='discussed-with-household-yes'))
+    js_click(driver, driver.find_element(by=By.ID, value='transportation-available-yes'))
+    js_click(driver, driver.find_element(by=By.ID, value='significant-changes-no'))
+    js_click(driver, driver.find_element(by=By.ID, value='room-for-foster-isolation-not-applicable'))
+
+    driver.find_element(by=By.ID, value='foster-limitations').send_keys('dairy')
+
+    date_input = driver.find_element(By.XPATH, '//input[@type="date"]')
+    driver.execute_script(f"arguments[0].value = '{date.today()}'", date_input)
+
+    action = ActionChains(driver)
+    canvas = driver.find_element(by=By.CSS_SELECTOR, value="canvas")
+    driver.execute_script("arguments[0].scrollIntoView();", canvas)
+    drawing = (action
+               .click_and_hold(canvas)
+               .move_by_offset(-10, -10)
+               .move_by_offset(15, 0)
+               .move_by_offset(0, 15)
+               .move_by_offset(-15, 0)
+               .move_by_offset(0, -15)
+               ).release()
+    drawing.perform()
+
+    submit = driver.find_element(by=By.XPATH, value='//input[@value="Send"]')
+    submit.click()
 
 
 def adopt(data: Dict) -> None:
