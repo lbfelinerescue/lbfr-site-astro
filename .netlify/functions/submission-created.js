8464e2984977ef836ce6332d82cf5ad41385c2b7
Daniel Baker <daniel.baker@tweag.io>
2023-06-21 18:18:43 -0700

    testing writing json file to gdrive

--- .netlify/functions/submission-created.js
+++ .netlify/functions/submission-created.js
@@ -1,8 +1,36 @@
 const PDFPrinter = require('pdfmake');
+const { google } = require('googleapis');
 
 
 exports.handler = async function (event, context) {
   console.log(JSON.parse(event.body));
+
+  const client = await google.auth.getClient({
+    credentials: JSON.parse(process.env.GDRIVE_CREDENTIALS),
+    scopes: 'https://www.googleapis.com/auth/drive.file',
+  })
+
+  const drive = google.drive({
+    version: 'v3',
+    auth: client,
+  })
+
+  const result = await drive.files.create({
+    requestBody: {
+      name: `form-submission-${(new Date()).toString()}.json`,
+      mimeType: 'text/json',
+      parents: [
+        // Folder ID of the Google Drive Share
+        // https://drive.google.com/drive/folders/{FOLDER_ID}
+        process.env.GDRIVE_FOLDER_ID
+      ],
+    },
+    media: {
+      mimeType: 'text/json',
+      body: JSON.stringify(event),
+    },
+  })
+
   return {
     statusCode: 200,
     body: JSON.stringify(event),
