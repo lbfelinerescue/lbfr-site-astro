d1bfc782056eacc46844a6c3f621371ee8cc5f16
David Nuon <davidnuongm@gmail.com>
2023-06-23 14:46:26 -0700

    Update contact for submission

--- .netlify/functions/forms/templates/contact.js
+++ .netlify/functions/forms/templates/contact.js
@@ -3,11 +3,12 @@ const { Heading, Paragraph, LineBreak } = require('../../components');
 function ContactTemplate(data) {
     return {
         content: [
-            Heading({ kind: 'h2', text: `From: ${data.name}` }),
+            Heading({ kind: 'h1', text: `Contact Form Submission` }),
+            Heading({ kind: 'h2', text: `From: ${data['contact-name']}` }),
             LineBreak(),
-            Heading({ kind: 'h2', text: `Email: ${data.email}` }),
+            Heading({ kind: 'h2', text: `Email: ${data['contact-email']}` }),
             LineBreak(),
-            Paragraph({ text: data.message }),
+            Paragraph({ text: data['contact-message'] }),
         ],
 
         defaultStyle: {
